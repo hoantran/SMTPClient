9ea98c2f35c515690be749f68e20b2251c4b5871
hoantran <hoan@bluepego.com>
2018-12-02 18:31:12 -0800

    * created simple client

--- SMTP/main.c
+++ SMTP/main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+#include "SMTPManager.h"
 
 int main(int argc, char *argv[]) {
+  struct SMTPClient *ptr = createSMTPClient();
+  printf("Socket #%d\n", ptr->sd);
 	return 0;
 }
