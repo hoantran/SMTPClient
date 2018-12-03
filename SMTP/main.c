f0e94ff3a1d62961bae95cce522fca76935ec41e
hoantran <hoan@bluepego.com>
2018-12-02 17:48:30 -0800

    first commit

---
+++ SMTP/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  SMTP
+//
+//  Created by Hoan Tran on 12/2/18.
+//  Copyright © 2018 Hoan Tran. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  printf("Hello, SMTP!\n");
+  return 0;
+}
